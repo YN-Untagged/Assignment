250f264184d068f1d003d43683a5a124496cc8de
YN-Untagged <89791536+YN-Untagged@users.noreply.github.com>
2022-02-23 13:52:06 +0200

    Add js

---
+++ js/site.js
@@ -0,0 +1,21 @@
+var num1 = Math.floor(Math.random() * 10);
+var num2 = Math.floor(Math.random() * 10);
+
+document.getElementById("question").innerText = "Answer  this: " + num1 + " + " + num2 + "=" ;
+
+function ShowNotification()
+{
+    document.getElementById("notify").innerText = "Message successfully delivered.";
+}
+
+function CheckAnswer(){
+
+    var ans = document.querySelector('#answer').value;
+
+    if(num1 + num2 == parseInt(ans)){
+        document.getElementById("submitBtn").style.display = "block";
+    }
+    else{
+        document.getElementById("notify").innerText = "Incorrect answer. Please try again."
+    }
+}
